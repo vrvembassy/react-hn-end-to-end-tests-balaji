1492a430766ef1a413aa9669a4b5df52a82c0887
KiranKumar-KV <31756754+KiranKumar-KV@users.noreply.github.com>
2018-01-21 21:31:55 +0530

    Delete test_localStorage.js

--- test/test_localStorage.js
+++
@@ -1,7 +0,0 @@
-describe('LocalStorage',function(){
-    it('should access localstorage contents',function(){
-        browser.url('');
-        browser.waitForVisible('.ListItem--loading', 10000 , true);
-        console.log('localStorage contents: \n',browser.localStorage());
-    });
-});
