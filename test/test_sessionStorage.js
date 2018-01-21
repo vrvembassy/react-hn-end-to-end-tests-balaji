d9d6a753a5819ef77d2f3ad53078c67318656b89
KiranKumar-KV <31756754+KiranKumar-KV@users.noreply.github.com>
2018-01-21 21:32:59 +0530

    Delete test_sessionStorage.js

--- test/test_sessionStorage.js
+++
@@ -1,7 +0,0 @@
-describe('Session Storage',function(){
-    it('should access sessionStorage contents',function(){
-        browser.url('');
-        browser.waitForVisible('.ListItem--loading', 10000 , true);
-        console.log('sessionStorage contents: \n',browser.sessionStorage());
-    });
-});
